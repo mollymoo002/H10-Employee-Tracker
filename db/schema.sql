742858bad6aef804dc576da959c6c86bf84c6b46
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-13 18:58:42 -0400

    removed a comma because the tables weren't poplulating

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE company_db;
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    name VARCHAR(30),
+    name VARCHAR(30)
 );
 
 DROP TABLE IF EXISTS _role;
