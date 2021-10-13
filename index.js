46a713cdb400571a59105b015c84f73dbee21a57
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-12 21:07:46 -0400

    added the empty if else statements in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// -------------- Require statements -----------------
 const inquirer = require('inquirer');
 let cTable = require('console.table');
+// -------------- Require statements -----------------
 
+// Here are the choices the user can search through using the arrow keys and pressing enter to choose one
 const questions = [
     {
         type: "list",
@@ -17,4 +20,35 @@ const questions = [
             "Quit"
         ]
     }
-]
+];
+
+// Quit the application
+function quit() {
+    process.exit();
+}
+
+async function init() {
+    console.log("Employee Tracker");
+    await inquirer
+        .prompt(questions)
+        .then ((response) => {
+            if (response.menuList === "View All Departments") {
+                
+            } else if (response.menuList === "View All Roles") {
+
+            } else if (response.menuList === "View All Employees") {
+                
+            } else if (response.menuList === "Add Department") {
+                
+            } else if (response.menuList === "Add Role") {
+                
+            } else if (response.menuList === "Add Employee") {
+                
+            } else if (response.menuList === "Update Employee Role") {
+                
+            } else {
+                return;
+            }
+            
+        })
+}
