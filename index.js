70d00af695d9578619d518eafdf0ba20dcc5f4cf
Molly Singmaster <mollysingmaster@gmail.com>
2021-10-13 21:12:20 -0400

    added content to the if statements

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // -------------- Require statements -----------------
 const inquirer = require('inquirer');
 let cTable = require('console.table');
+const query = require("./query/query");
 // -------------- Require statements -----------------
 
 // Here are the choices the user can search through using the arrow keys and pressing enter to choose one
@@ -33,23 +34,26 @@ async function init() {
         .prompt(questions)
         .then ((response) => {
             if (response.menuList === "View All Departments") {
-                
-            } else if (response.menuList === "View All Roles") {
-
-            } else if (response.menuList === "View All Employees") {
-                
-            } else if (response.menuList === "Add Department") {
-                
-            } else if (response.menuList === "Add Role") {
-                
-            } else if (response.menuList === "Add Employee") {
-                
-            } else if (response.menuList === "Update Employee Role") {
-                
-            } else {
-                return;
-            }
-            
+                query.listDept();
+            } 
+            if (response.menuList === "View All Roles") {
+                query.listEmp();
+            } 
+            if (response.menuList === "View All Employees") {
+                query.listRole();
+            } 
+            if (response.menuList === "Add Department") {
+                query.addDept();
+            } 
+            if (response.menuList === "Add Role") {
+                query.addRole();
+            } 
+            if (response.menuList === "Add Employee") {
+                query.addEmployee();
+            } 
+            if (response.menuList === "Update Employee Role") {
+                query.updateEmployeeRole();
+            }   
         })
 }
 init();
